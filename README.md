6d04012c91da8fc50acb82d7c349ac7562a0bfd3
Matt Carr <matthew.carr@rubikloud.com>
2020-10-09 12:06:26 -0400

    First working version

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # build-number
+
+Generates a build number of the format YYYY.MM.v.0, where:
+
+- YYYY.MM. is year.month, e.g. 2019.01.
+- v is the auto-incrementing version number. It will increment for every build in a given month, and then reset
+  after the month ends (so, 19.01.0.0, 19.01.1.0, 19.01.2.0, february starts, 19.02.0.0, 19.02.1.0, ...)
+- 0 is the patch number, which should be manually incremented as necessary
+
+This should be used for live applications, or in other instances where tags are needed but semver isn't the best solution
+
+Concept and large chunks of code from https://github.com/einaregilsson/build-number
--- main.js
+++ main.js
@@ -59,27 +59,6 @@ function request(method, path, data, callback) {
     }
 }
 
-/**
- * Requires a git repository to have already been checked out
- * Generates a build number of the format YYYY.MM.v.0, where:
- * - YY.MM. is year.month, e.g. 2019.01.
- * - v is the auto-incrementing version number. It will increment for every build in a given month, and then reset
- *   after the month ends (so, 19.01.0.0, 19.01.1.0, 19.01.2.0, february starts, 19.02.0.0, 19.02.1.0, ...)
- * - 0 is the patch number, which should be manually incremented as necessary
- *
- * This should be used for live applications, or in other instances where tags are needed but semver isn't
- * the best solution
- *
- * @return the next build number for the checked out repository
- */
-function generateBuildNumber(latestVersion) {
-    const [year, month, major, patch] = latestVersion.split('.').map(n => parseInt(n));
-    const currentDate = generateBuildDate();
-    const lastTaggedDate = `${year}.${month}`
-    return lastTaggedDate === currentDate ? `${currentDate}.${major + 1}.0` : `${currentDate}.0.0`
-}
-
-
 function generateBuildDate() {
     const o_date = new Intl.DateTimeFormat('en-us');
     const f_date = (m_ca, m_it) => Object({...m_ca, [m_it.type]: m_it.value});
@@ -87,11 +66,9 @@ function generateBuildDate() {
     return `${m_date.year}.${m_date.month}`; 
 }
 
-
 function main() {
 
     const path = 'BUILD_NUMBER/BUILD_NUMBER';
-    const prefix = env.INPUT_PREFIX ? `${env.INPUT_PREFIX}-` : '';
     const date = generateBuildDate();
 
     //See if we've already generated the build number and are in later steps...
@@ -113,16 +90,15 @@ function main() {
 
     request('GET', `/repos/${env.GITHUB_REPOSITORY}/git/refs/tags/${date}`, null, (err, status, result) => {
     
-        let nextBuildNumber, nrTags;
+        let nextBuildNumber;
     
         if (status === 404) {
             console.log('No existing tags for this month, starting at 0.');
             nextBuildNumber = `${date}.0.0`;
-            nrTags = [];
         } else if (status === 200) {
             const regexString = `/${date}\\.(\\d+)\\.(\\d+)$`;
             const regex = new RegExp(regexString);
-            nrTags = result.filter(d => d.ref.match(regex));
+            const nrTags = result.filter(d => d.ref.match(regex));
             
             //Existing build numbers:
             let nrs = nrTags.map(t => parseInt(t.ref.match(/\d+\.\d+\.(\d+)\.\d+$/)[1]));
